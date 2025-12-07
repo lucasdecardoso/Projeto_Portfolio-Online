bfa4f31c9e8ab46f242c2c3126a0a5788237a455
Lucas Cardoso <lucas_cardoso_de_castro@hotmail.com>
2025-12-07 15:06:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,13 @@ var mensagem =document.getElementById('mensagem');
 
 //executa o código somente ao clicar no botão
 botao.addEventListener('click', function() { 
-    if (nome.value != "" && email.value != "" && mensagem.value != "" && email.value.includes('@')){ //só envia se os campos estiverem preenchidos
-        alert('Sua mensagem foi enviada com sucesso!'); 
-    } else{
+    if (nome.value == "" || email.value == "" || mensagem.value == ""){ //só envia se os campos estiverem preenchidos
         alert('Todos os campos precisam estar preenchidos!');
+    } else if (!email.value.includes('@')){ //vai verificar se é um email válido
+        alert('Insira um e-mail válido!'); 
+    } else{
+        alert('Sua mensagem foi enviada com sucesso!'); 
     }
 });
+
 
