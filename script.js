7830c9a0681bac8b454b5a23a91cde082310fae2
Lucas Cardoso <lucas_cardoso_de_castro@hotmail.com>
2025-12-07 14:37:32 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+//traz os elementos do formulário para variáveis
+var botao = document.getElementById('botaoEnvio');
+var nome = document.getElementById('nome');
+var email =document.getElementById('email');
+var mensagem =document.getElementById('mensagem');
+
+//executa o código somente ao clicar no botão
+botao.addEventListener('click', function() { 
+    if (nome.value != "" && email.value != "" && mensagem.value != ""){ //só envia se os campos estiverem preenchidos
+        alert('Sua mensagem foi enviada com sucesso!'); 
+    } else{
+        alert('Todos os campos precisam estar preenchidos!');
+    }
+});
